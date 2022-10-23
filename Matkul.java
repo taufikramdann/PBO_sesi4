6e93ad446553f224a0d4141d9104bccbaadaa6ff
taufikramdann <115464057+taufikramdann@users.noreply.github.com>
2022-10-23 15:45:35 +0700

    Create Matkul.java

---
+++ Matkul.java
@@ -0,0 +1,12 @@
+kelas  publik Matkul  memperluas  Dosen {
+    String  nama ;
+
+    MataKulia ( String  nama ){
+        super ( nama );
+        ini . nama = nama ;
+    }
+
+     informasi kosong (){
+        Sistem . keluar . println ( "Mata Kuliah \t : " + this .nama ) ;
+    }
+}
