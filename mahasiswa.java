19580c536251c8766d0057ca63125aaee259e0c1
taufikramdann <115464057+taufikramdann@users.noreply.github.com>
2022-10-23 15:43:36 +0700

    Create mahasiswa.java

---
+++ mahasiswa.java
@@ -0,0 +1,11 @@
+kelas  umum Mahasiswa {
+    String  nama ;
+
+    Mahasiswa ( String  nama ){
+        ini . nama = nama ;
+    }
+
+     informasi kosong (){
+        Sistem . keluar . println ( "Nama Mahasiswa \t : " + this .nama ) ;
+    }
+}
