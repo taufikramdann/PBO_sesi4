47470e47c596cea26671bae040c71d254a6ae6fe
taufikramdann <115464057+taufikramdann@users.noreply.github.com>
2022-10-23 15:37:50 +0700

    Create Dosen.java

---
+++ Dosen.java
@@ -0,0 +1,12 @@
+public  class  Dosen  memperluas  Mahasiswa {
+    String  nama ;
+
+    Dosen ( String  nama ){
+        super ( nama );
+        ini . nama = nama ;
+    }
+
+     informasi kosong (){
+        Sistem . keluar . println ( "Nama Dosen \t\t : " + this .nama ) ;
+    }
+}
